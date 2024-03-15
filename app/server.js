438626f2c99576d6af401377be42dc40668db7bb
achauhan <chauhan.anurag@gmail.com>
2024-03-14 19:26:35 -0500

    added version

--- app/server.js
+++ app/server.js
@@ -1,9 +1,35 @@
 const express = require('express')
 const app = express()
 const port = 8080
+const fs = require('fs');
+
+// Function to read the value from the file
+function readFromFile(filePath) {
+    try {
+        // Read the content of the file synchronously
+        const content = fs.readFileSync(filePath, 'utf8');
+        return content.trim(); // Trim whitespace and newlines
+    } catch (err) {
+        console.error('Error reading file:', err);
+        return null;
+    }
+}
+
+// Path to the file
+const filePath = 'version.txt';
+
+// Read the value from the file
+const value = readFromFile(filePath);
+
+// Check if the value is not null
+if (value !== null) {
+    console.log('Value read from file:', value);
+} else {
+    console.log('Failed to read value from file.');
+}
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send(`Hello World! ${value}`)
 })
 
 app.listen(port, () => {
